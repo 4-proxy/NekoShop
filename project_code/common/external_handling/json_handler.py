a37129509e7084c915370be9cee9b860bbf06581
4-proxy <pubsec.4proxy.k91@outlook.com>
2024-09-21 16:33:01 +0900

    Cleaning up code, adding documentation.
    Major changes:
    - Edit `json_handler.py` 0.1.0 -> 1.0.0 (./project_code/common/external_handling/);
    - Edit `test_json_handler.py` 0.1.1 -> 1.0.0 (./project_code/common/tests/tests-external_handling/);
    
    Minor changes:
    - new `requirements.txt` (./project_code/windows/);
    - new `invalid_json_file.json` (./project_code/common/tests/tests-external_handling/test_data/);
    - rename `test_json_file.json` -> `valid_json_file.json` (./project_code/common/tests/tests-external_handling/test_data/);
    - edit `WhatNeed.txt` (./project_code/common/tests/tests-external_handling/test_data/);
    
    *Importantly:
    - close #10;

--- project_code/common/external_handling/json_handler.py
+++ project_code/common/external_handling/json_handler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-description
+The `json_handler` module is used to process JSON files.
 
 Copyright 2024 4-proxy
 Apache license, version 2.0 (Apache-2.0 license)
@@ -10,20 +10,37 @@ Apache license, version 2.0 (Apache-2.0 license)
 __all__: list[str] = ["get_content_from_json", "ContentJSON"]
 
 __author__ = "4-proxy"
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 import json
 
-from typing import Any, Dict
+from typing import Dict, Any
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class ContentJSON(Dict[str, Dict[str, Any]]):
+    """ContentJSON type describing the content of JSON file."""
     pass
 
 
 # -----------------------------------------------------------------------------
 def get_content_from_json(filepath: str) -> ContentJSON:
+    """get_content_from_json get JSON file content.
+
+    This function reads a JSON file from the specified file path and
+    parses its content into a Python dictionary.
+    *The resulting dictionary is wrapped in a custom type `ContentJSON`.
+
+    Args:
+        filepath (str): Path to `.json` file.
+
+    Returns:
+        ContentJSON: `.json` file content.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        json.JSONDecodeError: If the file content is not valid JSON.
+    """
     with open(file=filepath, mode='r') as json_file:
         file_content: Any = json.load(fp=json_file)
 
--- project_code/common/tests/tests-external_handling/test_json_handler.py
+++ project_code/common/tests/tests-external_handling/test_json_handler.py
@@ -1,55 +1,59 @@
 # -*- coding: utf-8 -*-
 
 """
-description
+Module `test_json_handler`, a set of test cases used to control the performance
+and quality of the `json_handler` module components.
 
 Copyright 2024 4-proxy
 Apache license, version 2.0 (Apache-2.0 license)
 """
 
 __author__ = "4-proxy"
-__version__ = "0.1.1"
+__version__ = "1.0.0"
 
 import unittest
+
+import os
 
 from common.external_handling import json_handler
 
 from typing import Dict
 
 
+CURRENT_FILE_PATH: str = os.path.abspath(path=__file__)
+CURRENT_DIR_PATH: str = os.path.dirname(p=CURRENT_FILE_PATH)
+TEST_DATA_DIR_NAME: str = "test_data"
+VALID_JSON_FILE_NAME: str = "valid_json_file.json"
+INVALID_JSON_FILE_NAME: str = "invalid_json_file.json"
+
+
 # _____________________________________________________________________________
 class TestGetContentFromJsonPositive(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        import os
-
         super().setUpClass()
-
-        current_dir: str = os.path.dirname(p=os.path.abspath(path=__file__))
-
-        cls._test_json_file_path: str = os.path.join(
-            current_dir, "test_data", "test_json_file.json"
+        cls._tested_json_file_path: str = os.path.join(
+            CURRENT_DIR_PATH, TEST_DATA_DIR_NAME, VALID_JSON_FILE_NAME
         )
 
     # -------------------------------------------------------------------------
     def setUp(self) -> None:
         super().setUp()
-
         self.tested_function = json_handler.get_content_from_json
         self.tested_specific_type = json_handler.ContentJSON
 
     # -------------------------------------------------------------------------
     def test_function_accept_filepath(self) -> None:
         # Build
-        test_filepath: str = self._test_json_file_path
+        test_filepath: str = self._tested_json_file_path
 
         # Check
         self.tested_function(test_filepath)
 
     # -------------------------------------------------------------------------
-    def test_returned_object_is_specific_type(self) -> None:
+    def test_returns_specific_type(self) -> None:
         # Build
-        test_filepath: str = self._test_json_file_path
+        test_filepath: str = self._tested_json_file_path
         expected_type = self.tested_specific_type
 
         # Operate
@@ -63,11 +67,11 @@ class TestGetContentFromJsonPositive(unittest.TestCase):
         )
 
     # -------------------------------------------------------------------------
-    def test_correct_reading_json_file_content(self) -> None:
+    def test_reads_json_file_content_correctly(self) -> None:
         # Build
-        test_filepath: str = self._test_json_file_path
+        test_filepath: str = self._tested_json_file_path
 
-        test_data: Dict[str, str] = {
+        expected_data: Dict[str, str] = {
             "key": "banana",
             "author": "4-proxy",
             "credit_card_code": "123"
@@ -77,34 +81,41 @@ class TestGetContentFromJsonPositive(unittest.TestCase):
         json_file_content = self.tested_function(test_filepath)
 
         # Check
-        subtest_counter: int = 0
-        for test_key, test_value in test_data.items():
-            subtest_counter += 1
-            with self.subTest(msg=f"Failure! Subtest {subtest_counter} is stopped!",
-                              pattern=(test_key, test_value)):
-                self.assertEqual(first=json_file_content.get(test_key),
-                                 second=test_value)
+        self.assertDictEqual(
+            d1=json_file_content,
+            d2=expected_data,
+            msg="Failure! The contents of inspected dictionary didn't match the expected!"
+        )
 
 
 # _____________________________________________________________________________
 class TestGetContentFromJsonNegative(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
-
         self.tested_function = json_handler.get_content_from_json
 
     # -------------------------------------------------------------------------
-    def test_if_file_not_exist_raise_FileNotFound(self) -> None:
-        import os
-
+    def test_FileNotFoundError_when_file_does_not_exist(self) -> None:
         # Build
-        current_dir: str = os.path.dirname(p=os.path.abspath(path=__file__))
-
         incorrect_filepath: str = os.path.join(
-            current_dir, "test_data", "file_not_exist.json"
+            CURRENT_DIR_PATH, TEST_DATA_DIR_NAME, "file_not_exist.json"
         )
 
         # Check
         with self.assertRaises(expected_exception=FileNotFoundError):
             # Operate
             self.tested_function(incorrect_filepath)
+
+    # -------------------------------------------------------------------------
+    def test_JSONDecodeError_on_invalid_json(self) -> None:
+        from json import JSONDecodeError
+
+        # Build
+        not_json_filepath = os.path.join(
+            CURRENT_DIR_PATH, TEST_DATA_DIR_NAME, INVALID_JSON_FILE_NAME
+        )
+
+        # Check
+        with self.assertRaises(expected_exception=JSONDecodeError):
+            # Operate
+            self.tested_function(not_json_filepath)
