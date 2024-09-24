c3d5c45989be6fbe31ef034837a4366bd0825dc6
4-proxy <pubsec.4proxy.k91@outlook.com>
2024-09-24 20:10:55 +0900

    Major changes: - Edit `logger_handler.py` 0.1.0 -> 0.2.0 (./project_code/common/external_handling/); - Edit `test_logger_handler.py` 0.1.0 -> 0.2.0 (./project_code/common/tests/tests-external_handling/);
    Minor changes:
    - edit `.gitignore` (./);
    
    *Importantly:
    - addresses #7;

--- project_code/common/external_handling/logger_handler.py
+++ project_code/common/external_handling/logger_handler.py
@@ -10,11 +10,13 @@ Apache license, version 2.0 (Apache-2.0 license)
 __all__: list[str] = [
     "LOG_FILENAME",
     "LOG_DIR_NAME",
-    "LOG_FILEPATH"
+    "LOG_FILEPATH",
+    "LOG_MESSAGE_FORMAT",
+    "LOG_DATE_FORMAT"
 ]
 
 __author__ = "4-proxy"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 import logging
 
@@ -25,6 +27,13 @@ LOG_FILENAME: str = "NekoShopApp.log"
 LOG_DIR_NAME: str = "logs"
 LOG_FILEPATH: str = os.path.join(LOG_DIR_NAME, LOG_FILENAME)
 
+LOG_MESSAGE_FORMAT: str = "%(asctime)s | %(levelname)s > %(message)s"
+LOG_DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S"
 
-logging.basicConfig(filename=LOG_FILEPATH,
-                    level=logging.NOTSET)
+
+logging.basicConfig(
+    filename=LOG_FILEPATH,
+    level=logging.NOTSET,
+    format=LOG_MESSAGE_FORMAT,
+    datefmt=LOG_DATE_FORMAT
+)
--- project_code/common/tests/tests-external_handling/test_logger_handler.py
+++ project_code/common/tests/tests-external_handling/test_logger_handler.py
@@ -8,7 +8,7 @@ Apache license, version 2.0 (Apache-2.0 license)
 """
 
 __author__ = "4-proxy"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 import unittest
 
@@ -18,30 +18,32 @@ import os
 import common.external_handling.logger_handler as logger_handler
 
 
-EXPECTED_LOG_FILENAME: str = "NekoShopApp.log"
-EXPECTED_LOG_DIR_NAME: str = "logs"
-EXPECTED_LOG_FILEPATH: str = os.path.join(EXPECTED_LOG_DIR_NAME,
-                                          EXPECTED_LOG_FILENAME)
-
-
 # _____________________________________________________________________________
 class TestLoggerHandler(unittest.TestCase):
+    EXPECTED_LOG_FILENAME: str = "NekoShopApp.log"
+    EXPECTED_LOG_DIR_NAME: str = "logs"
+    EXPECTED_LOG_FILEPATH: str = os.path.join(EXPECTED_LOG_DIR_NAME,
+                                              EXPECTED_LOG_FILENAME)
+    EXPECTED_LOG_MESSAGE_FORMAT: str = "%(asctime)s | %(levelname)s > %(message)s"
+    EXPECTED_LOG_DATE_FORMAT: str = "%Y-%m-%d %H:%M:%S"
+
+    # -------------------------------------------------------------------------
     def test_logger_file_name_is_correct(self) -> None:
         # Check
         self.assertEqual(first=logger_handler.LOG_FILENAME,
-                         second=EXPECTED_LOG_FILENAME)
+                         second=self.EXPECTED_LOG_FILENAME)
 
     # -------------------------------------------------------------------------
     def test_logger_directory_name_is_correct(self) -> None:
         # Check
         self.assertEqual(first=logger_handler.LOG_DIR_NAME,
-                         second=EXPECTED_LOG_DIR_NAME)
+                         second=self.EXPECTED_LOG_DIR_NAME)
 
     # -------------------------------------------------------------------------
     def test_logger_filepath_is_correct(self) -> None:
         # Check
         self.assertEqual(first=logger_handler.LOG_FILEPATH,
-                         second=EXPECTED_LOG_FILEPATH)
+                         second=self.EXPECTED_LOG_FILEPATH)
 
     # -------------------------------------------------------------------------
     def test_default_level_of_logger_is_NOTSET(self) -> None:
@@ -54,3 +56,15 @@ class TestLoggerHandler(unittest.TestCase):
         # Check
         self.assertEqual(first=logger.level,
                          second=expected_level)
+
+    # -------------------------------------------------------------------------
+    def test_logger_message_format_is_as_expected(self) -> None:
+        # Check
+        self.assertEqual(first=logger_handler.LOG_MESSAGE_FORMAT,
+                         second=self.EXPECTED_LOG_MESSAGE_FORMAT)
+
+    # -------------------------------------------------------------------------
+    def test_logger_date_format_is_as_expected(self) -> None:
+        # Check
+        self.assertEqual(first=logger_handler.LOG_DATE_FORMAT,
+                         second=self.EXPECTED_LOG_DATE_FORMAT)
